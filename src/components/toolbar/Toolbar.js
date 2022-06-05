4bfa2cb0dd9cf2df0cb04450ec46efb5af6c755b
Rauan <rkoshkinbayev@gmail.com>
2022-06-06 00:59:44 +0600

    finish base table logic

--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -4,10 +4,11 @@ export class Toolbar extends ExcelComponent {
 
     static className = 'excel__toolbar'
 
-    constructor($root) {
+    constructor($root, options) {
         super($root, {
             name: 'Toolbar',
-            listeners: ['click']
+            listeners: ['click'],
+            ...options
         });
     }
 
